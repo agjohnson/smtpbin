fb62019a4b211a7a7d4f82c3ea0eeade0f190b7a
Anthony Johnson <aj@ohess.org>
2013-05-30 01:32:07 -0700

    Add start of smtpd/policy server

---
+++ lib/SMTPbin/Daemon.pm
@@ -0,0 +1,48 @@
+package SMTPbin::Daemon;
+
+use 5.010;
+use strict;
+use warnings;
+
+use AnyEvent;
+use AnyEvent::Postfix::Policy;
+use AnyEvent::SMTP::Server;
+use Email::MIME;
+
+use SMTPbin::Model::Message;
+use SMTPbin::Model::Bin;
+
+
+sub mail {
+    my $class = shift;
+
+    # TODO make this all configurable
+    my $smtpd = AnyEvent::SMTP::Server(
+        host => undef,
+        port => 32001
+    );
+    $smtpd->reg_cb(
+        mail => \&recv_message
+    );
+
+    # TODO replace the polikcy server start mehtod to not block on recv
+    my $app = AnyEvent::Postfix::Policy->new();
+    $app->rule(
+        recipient => qr/\@smtpbin.org$/,
+        cb => sub { AnyEvent::Postfix::Policy::Response->new(
+            action => 'filter',
+            message => 'smtp:127.0.0.1:32001'
+        )}
+    );
+    $app->run(undef, 32002);
+
+    # TODO return a guard condvar?
+}
+
+sub recv_message {
+    my $mail = shift;
+    my $msg = SMTPbin::Model::Message->from_email($mail->{data});
+    $msg->save();
+}
+
+1;
